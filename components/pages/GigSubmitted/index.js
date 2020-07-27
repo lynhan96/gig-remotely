b7dd65398f9ff0a66c56d7fdb14b49e700cc553c
Nhan <lynhan15041997@gmail.com>
2020-07-28 00:04:10 +0700

    update time zone

--- components/pages/GigSubmitted/index.js
+++ components/pages/GigSubmitted/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Router from 'next/router';
 import {
-  Wrapper, Image, ActionWrapper, Title, StyledButton, StyledText, StyledLink,
+  Wrapper, Image, ActionWrapper, Title, StyledButton,
 } from './styles';
 
 const Verification = () => (
@@ -9,10 +9,6 @@ const Verification = () => (
     <Image src='/images/gigsumitted.png' />
     <Title>Your gig has been listed.</Title>
     <ActionWrapper>
-      <StyledText width='auto' size='sm'>
-        This also means half of what you paid has been given to the less fortunate! You can learn about gigremotely’s cause
-        <StyledLink> here.</StyledLink>
-      </StyledText>
       <StyledButton onClick={() => Router.push('/company/dashboard')}>view gig</StyledButton>
     </ActionWrapper>
   </Wrapper>
