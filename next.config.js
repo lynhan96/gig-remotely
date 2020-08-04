91fd45ed7e6abde5d1c63d9c33276f1aa2bc5c45
Nhan <lynhan15041997@gmail.com>
2020-08-04 22:46:47 +0700

    update env

--- next.config.js
+++ next.config.js
@@ -12,4 +12,12 @@ module.exports = withCSS({
     })
     return config;
   },
+  env: {
+    API_URL: 'https://api.gigremotely.com/',
+    FACEBOOK_ID: '1075658622827685',
+    GOOGLE_CLIENT_ID: '1034765245728-cvv2rfctlkn22u3jmkvmlh2bpt3hohc6.apps.googleusercontent.com',
+    LINKED_IN_REDIRECT_URL: 'https://gigremotely.com/linkedin',
+    LINKED_IN_CLIENT_ID: '86zxo5dqwfljd8',
+    STRIPE_CLIENT_ID: 'pk_live_l9NrmZnYXGraedzXYA4Qcmqk00z52LWv0P',
+  },
 });
