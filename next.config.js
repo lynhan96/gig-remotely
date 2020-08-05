7c8f40a3d9d7f867d092c34cb78a5c6e91f35e80
Nhan <lynhan15041997@gmail.com>
2020-08-05 12:15:07 +0700

    update config

--- next.config.js
+++ next.config.js
@@ -17,7 +17,7 @@ const envConfigs = {
     API_URL: 'https://api-staging.gigremotely.com/',
     FACEBOOK_ID: '612438743009066',
     GOOGLE_CLIENT_ID: '1011008869522-5ja3pn6a7jndtmbj5c2ps8plvb2vo8ai.apps.googleusercontent.com',
-    LINKED_IN_REDIRECT_URL: 'https://7c6ae87ccdbd.ngrok.io/linkedin',
+    LINKED_IN_REDIRECT_URL: 'https://gigremotely-staging.vercel.app/linkedin',
     LINKED_IN_CLIENT_ID: '863nizmny7l8qt',
     STRIPE_CLIENT_ID: 'pk_test_BIbbDbmvlENgzbbdBRuMsebI00awQs2uw5',
   },
