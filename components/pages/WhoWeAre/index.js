02442998be598088bcafe957d7d1950a39504743
Nhan <lynhan15041997@gmail.com>
2020-08-12 21:14:57 +0700

    update page who we are

--- components/pages/WhoWeAre/index.js
+++ components/pages/WhoWeAre/index.js
@@ -1,23 +1,13 @@
 import React from 'react';
 import {
-  Wrapper, Image, ContentWrapper, Title, WrapperText, StyledText,
+  Wrapper, Image, ContentWrapper, Title, WrapperText,
 } from './styles';
 
-const WhoWeAre = () => (
+const WhoWeAre = ({data}) => (
   <Wrapper>
     <Title>Who We Are</Title>
     <ContentWrapper>
-      <WrapperText>
-        <StyledText width='auto' size='sm'>
-          We believe that more and more companies are embarking on the modular and flexible hiring of workers. And with a never-ending demand for work, it’s about putting supply and demand together.
-        </StyledText>
-        <StyledText width='auto' size='sm'>
-          GigRemotely is here to simplify remote working; we connect the highest quality individuals to the right companies, regardless of where they are in the world and where they want to be in the world.
-        </StyledText>
-        <StyledText width='auto' size='sm'>
-          It is our responsibility to provide the crucial data on our GigSeekers; and from then on out, it’s down to the company to make the decisions and form their own dynamic team.
-        </StyledText>
-      </WrapperText>
+      <WrapperText dangerouslySetInnerHTML={{ __html: data.content }} />
       <Image src='/images/whoweare.png' />
     </ContentWrapper>
   </Wrapper>
--- pages/who-we-are.js
+++ pages/who-we-are.js
@@ -1,14 +1,29 @@
-import React from 'react';
-import Head from 'next/head';
-import { Container } from 'components/global/styles';
-import { WhoWeAre, Alert } from 'components/pages';
+import React, { useEffect, useCallback, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { Loading } from 'components/global';
+import { Container, LoadingWrapper } from 'components/global/styles';
+import { WhoWeAre } from 'components/pages';
+import { onGetSubpageContent } from 'saga/sub_page';
 
 const WhoWeArePage = () => {
+  const [state, setState] = useState({ loading: true, data: null });
+  const { loading, data } = state;
+  const dispatch = useDispatch();
+
+  const getContent = useCallback((page) => dispatch(
+    onGetSubpageContent(page, setState),
+  ), [dispatch]);
+
+  useEffect(() => {
+    getContent('who_we_are');
+  }, []);
+
   return (
     <>
       <Container>
-        <WhoWeAre />
-        <Alert />
+        { (loading)
+          ? (<LoadingWrapper><Loading showText size='60px' /></LoadingWrapper>)
+          : <WhoWeAre data={data} />}
       </Container>
     </>
   );
