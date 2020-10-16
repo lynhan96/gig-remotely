3035f59a005590281f3c12796679c09d7e1faae7
Nhan <lynhan15041997@gmail.com>
2020-10-16 16:29:15 +0700

    update meta

--- pages/_app.js
+++ pages/_app.js
@@ -43,7 +43,36 @@ const App = ({ Component, pageProps }) => {
           gtag("config", "UA-178267131-1");
         `,
         }} />
+        <script dangerouslySetInnerHTML={{
+        __html: `
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-5CZSW9T')
+        `,
+        }} />
+        <script dangerouslySetInnerHTML={{
+        __html: `
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '433990650905672');
+          fbq('track', 'PageView');
+        `,
+        }} />
+
+        <noscript><img height="1" width="1" style={{display:"none"}}
+        src="https://www.facebook.com/tr?id=433990650905672&ev=PageView&noscript=1"
+        /></noscript>
       </Head>
+      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5CZSW9T"
+      height="0" width="0" style={{display: "none",visibility: "hidden"}}></iframe></noscript>
       <Header />
       <MainLayout Component={Component} pageProps={pageProps} />
       <Footer />
